362e5a30f5962fe2b621812a29fadd49eb70b37e
Chirpatel <32012234+Chirpatel@users.noreply.github.com>
2020-06-20 23:03:36 +0530

    Loading Gif

--- Files/script.js
+++ Files/script.js
@@ -1,7 +1,21 @@
+function fadeout(){
+    $(".preload").fadeOut(1000);
+    move();
+}
 function toggle(){
     $(".fa-bars").toggleClass("fa-times");
 }
-
+function move(){
+    var hash = '#'+window.location.hash.substr(1);
+    console.log(hash);
+    if (hash !== "#") {
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+          window.location.hash = hash;
+        });
+      }
+}
 $(document).ready(function(){
     $("a").on('click', function(event) {
       if (this.hash !== "") {
@@ -15,3 +29,4 @@ $(document).ready(function(){
       }
     });
   });
+
